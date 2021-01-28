940560cebe87ecb0fb3add56870062a64651fa77
adamnemecek <adamnemecek@gmail.com>
2021-01-28 09:53:51 -0800

    made function public lol

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![feature(const_raw_ptr_deref)]
 
-const fn four_cc(cc: &[u8; 4]) -> u32 {
+pub const fn four_cc(cc: &[u8; 4]) -> u32 {
     let v = unsafe { *(cc.as_ptr() as *const u32) };
     #[cfg(target_endian="little")] {
         v.to_be()
@@ -10,10 +10,9 @@ const fn four_cc(cc: &[u8; 4]) -> u32 {
     }
 }
 
-// #[cfg(test)]
-
+#[cfg(test)]
 mod tests {
-    use super::four_cc;
+    use crate::four_cc;
     // from https://gist.github.com/rust-play/730673365471818b5bbbc876e65ffe40
     #[test]
     fn test_a_thing() {
